81dc03b7153519922f65b4dd893aa103b53ecd23
Jaba <jabaaladashvili1992@gmail.com>
2022-11-24 14:35:21 +0400

    initial commit

---
+++ validation.js
@@ -0,0 +1,28 @@
+// validation script here
+let reg = /[a-z]/gi;
+// let reg2 = new RegExp(/[a-z]/, "gi");
+const inputs = document.querySelectorAll("input");
+const patterns = {
+ telephone: /^\d{11}$/,
+ username: /^[a-z\d]{5,12}$/i,
+ password: /^[\w@-]{8,20}$/,
+ slug: /^[a-z\d-]{8,20}$/,
+ email: /^([a-z\d\.-]+)@([a-z\d\-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+}
+
+function validate(field, regex) {
+ if (regex.test(field.value)) {
+  field.className = "valid"
+
+ } else {
+  field.className = "invalid"
+
+ }
+}
+
+inputs.forEach((item) => {
+ item.addEventListener("keyup", (e) => {
+  //console.log(e.target.attributes.name.value);
+  validate(e.target, patterns[e.target.attributes.name.value])
+ })
+})
